44b3384914db6c6da46eaa8125ef8fc715f657e5
Steellkek <alya.min80@gmail.com>
2022-03-18 18:02:36 +0300

    Initial commit

---
+++ Bars_Group_Event/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bars_Group_Event
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var key = new Key();
+            key.OnKeyPressed += (Sender,ButtonKey) => Console.WriteLine("\n"+ButtonKey);
+            
+            key.Run();
+        }
+
+        internal class Key
+        {
+            public event EventHandler<char>? OnKeyPressed;
+            public void Run()
+            {
+                char i = Console.ReadKey().KeyChar;
+                while ((char.ToLower(i) != 'c') & (char.ToLower(i) != 'с') )
+                {
+                    OnKeyPressed?.Invoke(this,i);
+                    i = Console.ReadKey().KeyChar;
+                }
+            }
+        }
+    }
+}
